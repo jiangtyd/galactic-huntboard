acbd236404c45ab41a5b31cb02ee7494030db1b5
Jon Schneider <jschnei@Jons-MacBook-Air-2.local>
2013-12-30 22:16:42 -0500

    replaced GAE/Flask skeleton with GAE + Google Drive template

--- main.py
+++ main.py
@@ -1,21 +1,136 @@
-""" main.py is the top level script.
+#!/usr/bin/env python
+#
+# Copyright 2013 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+"""Starting template for Google App Engine applications.
 
-Return "Hello World" at the root URL.
+Use this project as a starting point if you are just beginning to build a Google
+App Engine project. Remember to download the OAuth 2.0 client secrets which can
+be obtained from the Developer Console <https://code.google.com/apis/console/>
+and save them as 'client_secrets.json' in the project directory.
 """
 
+import httplib2
+import json
+import logging
 import os
-import sys
+import random
 
-# sys.path includes 'server/lib' due to appengine_config.py
-from flask import Flask
-from flask import render_template
-app = Flask(__name__.split('.')[0])
+from apiclient import discovery
+from oauth2client import appengine
+from oauth2client import client
 
+from google.appengine.api import memcache
+from google.appengine.ext import db
 
-@app.route('/')
-@app.route('/<name>')
-def hello(name=None):
-  """ Return hello template at application root URL."""
-  return render_template('hello.html', name=name)
+import webapp2
+import jinja2
 
 
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')),
+    autoescape=True,
+    extensions=['jinja2.ext.autoescape'])
+
+# CLIENT_SECRETS, name of a file containing the OAuth 2.0 information for this
+# application, including client_id and client_secret, which are found
+# on the API Access tab on the Google APIs
+# Console <http://code.google.com/apis/console>
+CLIENT_SECRETS = os.path.join(os.path.dirname(__file__), 'client_secrets.json')
+
+# Helpful message to display in the browser if the CLIENT_SECRETS file
+# is missing.
+MISSING_CLIENT_SECRETS_MESSAGE = """
+<h1>Warning: Please configure OAuth 2.0</h1>
+<p>
+To make this sample run you will need to populate the client_secrets.json file
+found at:
+</p>
+<p>
+<code>%s</code>.
+</p>
+<p>with information found on the <a
+href="https://code.google.com/apis/console">APIs Console</a>.
+</p>
+""" % CLIENT_SECRETS
+
+
+http = httplib2.Http(memcache)
+service = discovery.build('drive', 'v2', http=http)
+decorator = appengine.oauth2decorator_from_clientsecrets(
+    CLIENT_SECRETS,
+    scope=[
+      'https://www.googleapis.com/auth/drive',
+      'https://www.googleapis.com/auth/drive.appdata',
+      'https://www.googleapis.com/auth/drive.apps.readonly',
+      'https://www.googleapis.com/auth/drive.file',
+      'https://www.googleapis.com/auth/drive.metadata.readonly',
+      'https://www.googleapis.com/auth/drive.readonly',
+      'https://www.googleapis.com/auth/drive.scripts',
+    ],
+    message=MISSING_CLIENT_SECRETS_MESSAGE)
+
+
+class MainHandler(webapp2.RequestHandler):
+
+  @decorator.oauth_aware
+  def get(self):
+    variables = {
+        'url': decorator.authorize_url(),
+        'has_credentials': decorator.has_credentials(),
+        }
+        
+    if decorator.has_credentials():
+        self.redirect('/files')
+    else:
+        template = JINJA_ENVIRONMENT.get_template('main.html')
+        self.response.write(template.render(variables))
+
+class FilesHandler(webapp2.RequestHandler):
+  @decorator.oauth_required
+  def get(self):
+    response = service.files().list().execute(decorator.http())
+    items = response.get('items')
+    files = [{'title':item.get('title'),
+              'link':item.get('defaultOpenWithLink')} for item in items]
+
+    print json.dumps(response, sort_keys=True, indent=4) # for debugging
+    
+    variables = {
+        'files':files
+    }
+    template = JINJA_ENVIRONMENT.get_template('files.html')
+    self.response.write(template.render(variables))
+
+class MakeFileHandler(webapp2.RequestHandler):
+  @decorator.oauth_required
+  def get(self):
+  
+    body = {
+      'mimeType': 'application/vnd.google-apps.spreadsheet',
+      'title': 'Puzzle %i' % random.randint(0, 10000),
+    }
+    response = service.files().insert(body=body).execute(decorator.http())
+  
+    self.redirect('/files')
+
+app = webapp2.WSGIApplication(
+    [
+     ('/', MainHandler),
+     ('/files', FilesHandler),
+     ('/makefile', MakeFileHandler),
+     (decorator.callback_path, decorator.callback_handler()),
+    ],
+    debug=True)
