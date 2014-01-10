768172e6a31d02d45488b3d1ac60a31962f7eb72
DD Liu <liudi@mit.edu>
2014-01-10 04:20:44 -0800

    Authentication, needs testing still since auth requires exact URL

--- main.py
+++ main.py
@@ -23,18 +23,21 @@ and save them as 'client_secrets.json' in the project directory.
 """
 
 import httplib2
+import jinja2
+import json
+import logging
 import os
 import pickle
 import random
+import urllib2
+import webapp2
 
 from apiclient import discovery
-
-from google.appengine.api import memcache
-
-import webapp2
-import jinja2
-import json
-
+from base import AuthHandler, BaseHandler
+from google.appengine.api import memcache, urlfetch
+from oauth2client.appengine import xsrf_secret_key
+from oauth2client.client import OAuth2WebServerFlow
+from webapp2_extras import sessions
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(
@@ -44,6 +47,16 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 
 DRIVE = discovery.build('drive', 'v2', http=httplib2.Http())
 
+# Create a flow object. This object holds the client_id, client_secret, and
+# scope. It assists with OAuth 2.0 steps to get user authorization and
+# credentials. For this example, the client ID and secret are command-line
+# arguments.
+flow = OAuth2WebServerFlow('994323699569-rhgb05bmjknl5megejrhq0ej5rg2n9v8.apps.googleusercontent.com',
+                         '9HMsRtgNWF1ZM1I6WkVkmLuJ',
+                         [],
+                         redirect_uri='http://galactichuntboard.appspot.com/oauth2callback')
+
+# ('https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/apps.groups.settings')
 
 def get_credentials():
     credentials = memcache.get('credentials')
@@ -67,24 +80,22 @@ def getFiles():
 def makeFile(name):
     body = {
       'mimeType': 'application/vnd.google-apps.spreadsheet',
-      'title': name
+      'title': name,
+      'parents': [    
+            { # A reference to a file's parent.
+              "isRoot": False, # Whether or not the parent is the root folder.
+              "kind": "drive#parentReference", # This is always drive#parentReference.
+              "id": "0B1zTSYJ9kTiqdlNwOGZvUmtRNnc", # The ID of the parent.
+              "selfLink": "https://content.googleapis.com/drive/v2/files/1SBOd6WT7MAHUSGwsOF_nXOrkR_EONR11Ls5L8AC8O0c/parents", # A link back to this reference.
+              "parentLink": "https://content.googleapis.com/drive/v2/files/0B1zTSYJ9kTiqdlNwOGZvUmtRNnc", # A link to the parent.
+            }
+        ]
     }
 
     file_id = DRIVE.files().insert(body=body).execute(auth_http()).get('id')
 
-    perm = {
-        'withLink': True,
-        'role': "writer",
-        'type': "anyone",
-        'value': ''
-    }
 
-    DRIVE.permissions().insert(
-        fileId=file_id,
-        body=perm).execute(auth_http())
-
-
-class MainHandler(webapp2.RequestHandler):
+class MainHandler(AuthHandler):
 
     def get(self):
         variables = {
@@ -93,17 +104,91 @@ class MainHandler(webapp2.RequestHandler):
         template = JINJA_ENVIRONMENT.get_template('files.html')
         self.response.write(template.render(variables))
 
-class FileHandler(webapp2.RequestHandler):
 
+class LoginHandler(BaseHandler):
+
+    def get(self):
+
+        logging.info("%s", str('loginll'))
+        authurl = 'https://accounts.google.com/o/oauth2/auth?' + \
+            'client_id=994323699569-rhgb05bmjknl5megejrhq0ej5rg2n9v8.apps.googleusercontent.com&' + \
+            'response_type=code&' + \
+            'scope=openid%20email&' + \
+            'redirect_uri=http://galactichuntboard.appspot.com/oauth2callback&' + \
+            'login_hint='+self.getEmail()+'&' + \
+            'state='+ xsrf_secret_key()
+        try:
+            self.redirect(authurl)
+        except:
+            self.redirect('/loginfailed')
+
+
+class LoginCallbackHandler(BaseHandler):
+    def get(self):
+
+        # Ensure that the request is not a forgery and that the user sending
+        # this connect request is the expected user.
+
+        try:
+            if self.request.get_all('state') != appengine.xsrf_secret_key():
+                self.redirect('/loginfailed')
+
+            code = self.request.get_all('code')
+            auth = RequestHandler.flow.step2_exchange(code)
+            session['auth']=credentials
+        except:
+            self.redirect("/loginfailed")
+
+        allowed = urllib2.urlopen('https://www.googleapis.com/groups/v1/groups/galactic-dogesetters@gmail.com?fields=email')
+        allowed = json.loads(allowed)
+
+        if not (self.session.get('auth').id_token.email in allowed):
+            self.redirect("/loginfailed")
+
+
+        template = JINJA_ENVIRONMENT.get_template('login-complete.html')
+        self.response.write(template.render())
+
+
+class LoginFailedHandler(BaseHandler):
+    def get(self):
+        logging.info("%s", str('loginfailed'))
+        variables = {
+            'username': getEmail()
+        }
+        template = JINJA_ENVIRONMENT.get_template('login-failed.html')
+        self.response.write(template.render({ 'username': getEmail() }))
+
+
+class LogoutHandler(BaseHandler):
     def get(self):
         template = JINJA_ENVIRONMENT.get_template('_filelist.html')
 
         self.response.headers['Content-Type'] = 'application/json'
         self.response.write(json.dumps(
             { 'html': template.render({ 'files': getFiles() }) }
-            ))
+            )
+        )
 
-class MakeFileHandler(webapp2.RequestHandler):
+#        logging.info("%s", str('logout'))
+#        self.session["auth"] = ''
+
+#        template = JINJA_ENVIRONMENT.get_template('logout.html')
+#        self.response.write(template.render())
+
+
+class FileHandler(AuthHandler):
+    def get(self):
+        template = JINJA_ENVIRONMENT.get_template('_filelist.html')
+
+        self.response.headers['Content-Type'] = 'application/json'
+        self.response.write(json.dumps(
+            { 'html': template.render({ 'files': getFiles() }) }
+            )
+        )
+
+
+class MakeFileHandler(AuthHandler):
 
     def post(self):
         makeFile(self.request.get('name'))
@@ -113,9 +198,11 @@ class MakeFileHandler(webapp2.RequestHandler):
         self.response.headers['Content-Type'] = 'application/json'
         self.response.write(json.dumps(
             { 'html': template.render({ 'files': getFiles() }) }
-            ))
+            )
+        )
 
-class ClearAllHandler(webapp2.RequestHandler):
+
+class ClearAllHandler(AuthHandler):
 
     def get(self):
         http = auth_http()
@@ -129,9 +216,17 @@ class ClearAllHandler(webapp2.RequestHandler):
 
 routes = [
         (r'/', MainHandler),
+        (r'/login', LoginHandler),
+        (r'/loginfailed', LoginFailedHandler),
+        (r'/oauth2callback', LoginCallbackHandler),
+        (r'/leave', LogoutHandler),
         (r'/files', FileHandler),
         (r'/files/new', MakeFileHandler),
         (r'/clearall', ClearAllHandler),
         ]
 
-app = webapp2.WSGIApplication(routes=routes, debug=True)
+config = {}
+config['webapp2_extras.sessions'] = {
+    'secret_key': '3t4yjumn2be8e2a6iwvqnkod54y4s89wb5fd',
+}
+app = webapp2.WSGIApplication(routes=routes, debug=True, config=config)
