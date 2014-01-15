629c86e4b74eec77e9fbc3a74dfdc0e9cac94427
DD Liu <liudi@mit.edu>
2014-01-15 09:13:20 -0800

    Let's show the status bar in the chat window. It looks like it has some useful options.

--- javascripts/main_puzzle.js
+++ javascripts/main_puzzle.js
@@ -1,19 +1,20 @@
 'use strict';
 
 $( document ).ready(function() {
-    $('#spreadsheetWrapper').height($(window).height()-$("#chatWrapper").height());
+    var CHAT_STATUS_BAR_HEIGHT = 20;
+    $('#spreadsheetWrapper').height($(window).height()-$("#chatWrapper").height()-CHAT_STATUS_BAR_HEIGHT);
 
     window.onresize = function(event) {
-        $('#spreadsheetWrapper').height($(window).height()-$("#chatWrapper").height());
+        $('#spreadsheetWrapper').height($(window).height()-$("#chatWrapper").height()-CHAT_STATUS_BAR_HEIGHT);
     }
 
     $('#chatWrapper').resizable({
         alsoResize: '#chat',
         alsoResizeReverse: '#spreadsheetWrapper',
         handles: 's',
-        minHeight: 25,
+        minHeight: 80,
         resize: function() {
-            $('#spreadsheetWrapper').height($(window).height()-$("#chatWrapper").height()-$("callins").height());
+            $('#spreadsheetWrapper').height($(window).height()-$("#chatWrapper").height()-$("callins").height()-CHAT_STATUS_BAR_HEIGHT);
         },
         start: function() {
             $('#chatWrapper').addClass('noMouseEvents');
